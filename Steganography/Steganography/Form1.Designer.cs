d725c17d563628c4865cf73b95ccaf4b6e97b065
GokhanK0 <170205052@kku.edu.tr>
2019-07-25 17:14:38 +0300

    ilk gönderme

---
+++ Steganography/Steganography/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Steganography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct_image = new System.Windows.Forms.PictureBox();
+            this.btn_open_file = new System.Windows.Forms.Button();
+            this.btn_decode = new System.Windows.Forms.Button();
+            this.btn_encode = new System.Windows.Forms.Button();
+            this.txt_file_path = new System.Windows.Forms.TextBox();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct_image
+            // 
+            this.pct_image.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pct_image.Location = new System.Drawing.Point(109, 22);
+            this.pct_image.Name = "pct_image";
+            this.pct_image.Size = new System.Drawing.Size(564, 309);
+            this.pct_image.TabIndex = 0;
+            this.pct_image.TabStop = false;
+            // 
+            // btn_open_file
+            // 
+            this.btn_open_file.Location = new System.Drawing.Point(173, 354);
+            this.btn_open_file.Name = "btn_open_file";
+            this.btn_open_file.Size = new System.Drawing.Size(75, 23);
+            this.btn_open_file.TabIndex = 1;
+            this.btn_open_file.Text = "Open File";
+            this.btn_open_file.UseVisualStyleBackColor = true;
+            this.btn_open_file.Click += new System.EventHandler(this.Btn_open_file_Click);
+            // 
+            // btn_decode
+            // 
+            this.btn_decode.Location = new System.Drawing.Point(420, 461);
+            this.btn_decode.Name = "btn_decode";
+            this.btn_decode.Size = new System.Drawing.Size(75, 38);
+            this.btn_decode.TabIndex = 1;
+            this.btn_decode.Text = "Decode";
+            this.btn_decode.UseVisualStyleBackColor = true;
+            this.btn_decode.Click += new System.EventHandler(this.Btn_decode_Click);
+            // 
+            // btn_encode
+            // 
+            this.btn_encode.Location = new System.Drawing.Point(296, 461);
+            this.btn_encode.Name = "btn_encode";
+            this.btn_encode.Size = new System.Drawing.Size(75, 38);
+            this.btn_encode.TabIndex = 1;
+            this.btn_encode.Text = "Encode";
+            this.btn_encode.UseVisualStyleBackColor = true;
+            this.btn_encode.Click += new System.EventHandler(this.Btn_encode_Click);
+            // 
+            // txt_file_path
+            // 
+            this.txt_file_path.Location = new System.Drawing.Point(273, 357);
+            this.txt_file_path.Name = "txt_file_path";
+            this.txt_file_path.Size = new System.Drawing.Size(181, 20);
+            this.txt_file_path.TabIndex = 2;
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(273, 435);
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(248, 20);
+            this.txt_message.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Message :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 581);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.txt_file_path);
+            this.Controls.Add(this.btn_encode);
+            this.Controls.Add(this.btn_decode);
+            this.Controls.Add(this.btn_open_file);
+            this.Controls.Add(this.pct_image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pct_image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct_image;
+        private System.Windows.Forms.Button btn_open_file;
+        private System.Windows.Forms.Button btn_decode;
+        private System.Windows.Forms.Button btn_encode;
+        private System.Windows.Forms.TextBox txt_file_path;
+        private System.Windows.Forms.TextBox txt_message;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Steganography/Steganography/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steganography
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_open_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opendialog = new OpenFileDialog();
+            opendialog.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+            opendialog.InitialDirectory = @"D:\SteganProje";
+            if (opendialog.ShowDialog() == DialogResult.OK)
+            {
+                txt_file_path.Text = opendialog.FileName.ToString();
+                pct_image.ImageLocation = txt_file_path.Text;
+            }
+        }
+
+        private void Btn_encode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(txt_file_path.Text);
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < txt_message.TextLength)
+                    {
+                        Console.WriteLine("R = [" + i + "][" + j + "] = " + pixel.R);
+                        Console.WriteLine("G = [" + i + "][" + j + "] = " + pixel.G);
+                        Console.WriteLine("B = [" + i + "][" + j + "] = " + pixel.B);
+
+                        char letter = Convert.ToChar(txt_message.Text.Substring(j, 1));
+                        int value = Convert.ToInt32(letter);
+                        Console.WriteLine("letter : " + letter + " value : " + value);
+
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
+                    }
+                    if (i == img.Width - 1 && j == img.Height -1)
+                    {
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, txt_message.TextLength));
+
+                    }
+                }
+            }
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+            saveFile.InitialDirectory = @"D:\SteganProje";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                txt_file_path.Text = saveFile.FileName.ToString();
+                pct_image.ImageLocation = txt_file_path.Text;
+
+                img.Save(txt_file_path.Text);
+            }
+        }
+
+        private void Btn_decode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(txt_file_path.Text);
+
+            string message = "";
+
+            Color lastpixel = img.GetPixel(img.Width - 1, img.Height - 1);
+            int msgLength = lastpixel.B;
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < msgLength)
+                    {
+
+                        int value = pixel.B;
+                        char c = Convert.ToChar(value);
+                        string letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+
+                        message = message + letter;
+
+                    }
+                }
+            }
+            txt_message.Text = message; 
+        }
+    }
+}
